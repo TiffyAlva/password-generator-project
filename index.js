b09967d35dffa5b6feebce2aae199a298ed49ffa
TiffyAlva <TiffanyAlvarez54@gmail.com>
2023-08-06 15:15:33 -0400

    added copy clipboard

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+const characters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
 
@@ -25,6 +25,34 @@ generateBtn.addEventListener("click",()=>{
         fieldOne.textContent = generatePassword()
         fieldTwo.textContent = generatePassword()
 })
+
+// Copy Password
+function copyToClipboard(fieldId) {
+    const targetField = document.getElementById(fieldId);
+    if (targetField) {
+        const range = document.createRange();
+        range.selectNode(targetField);
+        window.getSelection().removeAllRanges();
+        window.getSelection().addRange(range);
+
+        try {
+            const successful = document.execCommand('copy');
+            if (successful) {
+                // Password successfully copied to clipboard
+                alert('Password copied to clipboard!');
+            } else {
+                // Copy command failed
+                alert('Copy command failed. Please copy the password manually.');
+            }
+        } catch (err) {
+            // Handle any errors that may occur during the copy process
+            console.error('Copy to clipboard failed:', err);
+            alert('Copy to clipboard failed. Please copy the password manually.');
+        } finally {
+            window.getSelection().removeAllRanges();
+        }
+    }
+}
 
 
 
